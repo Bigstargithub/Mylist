9219ce4862741369cd0bc9d6fcb44c019a18438f
Bigstargithub <qufquf2004@hanmail.net>
2019-01-18 23:52:38 +0900

    I did it!

--- app/src/main/java/edu/handong/hisnet/mylist/MainActivity.java
+++ app/src/main/java/edu/handong/hisnet/mylist/MainActivity.java
@@ -2,54 +2,53 @@ package edu.handong.hisnet.mylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    static final String[] LIST_MENU = {"창세기","출애굽기","레위기","민수기","신명기","여호수아","사사기",
-                                       "룻기","사무엘상","사무엘하","열왕기상","열왕기하","역대상","역대하",
-                                       "에스라","느헤미야","에스더","욥기","시편","잠언","전도서","아가",
-                                       "이사야","예레미야","예레미야애가","에스겔","다니엘","호세아","요엘",
-                                       "아모스","오바댜","요나","미가","나훔","하박국","스바냐","학개","스가랴",
-                                       "말라기","마태복음","마가복음","누가복음","요한복음","사도행전","로마서",
-                                       "고린도전서","고린도후서","갈라디아서","에베소서","빌립보서","골로새서",
-                                       "데살로니가전서","데살로니가후서","디모데전서","디모데후서","디도서",
-                                       "빌레몬서","히브리서","야고보서","베드로전서","베드로후서","요한1서",
-                                       "요한2서","요한3서","유다서","요한계시록"};
-    //private ArrayList<HashMap<String,String>> Data = new ArrayList<HashMap<String, String>>();
-    //private HashMap<String,String> InputData1 = new HashMap<>();
-   // private HashMap<String,String> InputData2 = new HashMap<>();
 
+    private TextView BibleVerse;
+    public static final int[] numOfChapters ={50,40,27,36,34,24,21,4,31,24,
+            22,25,29,36,10,13,10,42,150,31,
+            12,8,66,52,5,48,12,14,3,9,
+            1,4,7,3,3,3,2,14,4,
+            28,16,24,21,28,16,16,13,6,6,
+            4,4,5,3,6,4,3,1,13,5,
+            5,3,5,1,1,1,22};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //BibleList를 ArrayAdapter에 넣는다.
+        final String[] Biblelist = getResources().getStringArray(R.array.Biblelist);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,LIST_MENU);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, Biblelist);
 
         ListView listView = (ListView) findViewById(R.id.List_view);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                    String selected_item = String.valueOf(adapterView.getItemAtPosition(i));
+                    Toast.makeText(MainActivity.this, selected_item + "는 " + numOfChapters[i]+"입니다", Toast.LENGTH_LONG).show();
+
+                }
+        });
         listView.setAdapter(adapter);
-        //데이터 초기화
-        /*InputData1.put("school","서울대");
-        InputData1.put("name","유혁");
-        Data.add(InputData1);
 
-        InputData2.put("school","연세대");
-        InputData2.put("name", "유재석");
-        Data.add(InputData2);
 
-        //simpleAdapter 생성
-        SimpleAdapter simpleAdapter = new
-                SimpleAdapter(this,Data,android.R.layout.simple_list_item_1,
-                ,new int[]{android.R.id.text1,android.R.id.text2});
-        listView.setAdapter(simpleAdapter);
-*/
+
 
     }
 }
+
+
+
+
